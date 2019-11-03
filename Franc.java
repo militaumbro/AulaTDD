ce4214fb780880b729ed4c82e758e15009f9aee6
militaumbro <39736533+militaumbro@users.noreply.github.com>
2019-11-03 11:17:01 -0300

    COMMIT 2 REAL

--- Franc.java
+++ Franc.java
@@ -1,8 +1,3 @@
-public void testFrancMultiplication() {
-   Franc five = new Franc(5);
-   assertEquals(new Franc(10), five.times(2));
-   assertEquals(new Franc(15), five.times(3));
-}
 class Franc {   
    Franc(int amount) {      
       this.amount= amount;
